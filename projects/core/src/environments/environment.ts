76def853e8956135c5358ae2e4f32d0d228e94f4
Bogdan Daniel <daniel_bogdan7@yahoo.com>
2026-04-20 17:57:01 +0300

    env update

--- projects/core/src/environments/environment.ts
+++ projects/core/src/environments/environment.ts
@@ -1,10 +1,7 @@
 export const environment = {
   production: true,
   appUrl: 'https://beeactive-dev.fit',
-  apiUrl: 'http://localhost:3800',
-  //apiUrl: 'https://beeactive-api-production.up.railway.app',
-  // production: false,
-
+  apiUrl: 'https://beeactive-api-production.up.railway.app',
   googleClientId: '119425399334-29l3eq2mo162t0vlh8qfoqgi2cg0djfp.apps.googleusercontent.com',
   facebookAppId: '888056193830836',
 };
