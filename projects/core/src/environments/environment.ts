4e3981880a8374cbe062bd8ce04044389c5224f6
Bogdan Daniel <daniel_bogdan7@yahoo.com>
2026-04-20 18:44:25 +0300

    auth and error pages update

--- projects/core/src/environments/environment.ts
+++ projects/core/src/environments/environment.ts
@@ -4,4 +4,5 @@ export const environment = {
   apiUrl: 'https://beeactive-api-production.up.railway.app',
   googleClientId: '119425399334-29l3eq2mo162t0vlh8qfoqgi2cg0djfp.apps.googleusercontent.com',
   facebookAppId: '888056193830836',
+  // facebookAppId: '888056056193830836',
 };
